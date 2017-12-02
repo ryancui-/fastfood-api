c121595870f86ffbf408ad7065e9798f8256f639
ryancui- <cuizhaowei92@gmail.com>
2017-12-02 15:43:08 +0800

    feat: 提供不同类型查询订单团列表

--- src/controller/group.js
+++ src/controller/group.js
@@ -42,14 +42,37 @@ module.exports = class extends Base {
 
   // 获取所有订单组
   async listAction() {
-    // 按创建时间倒序排列，只列出征集中的团组
-    const groupList = await this.model('group')
-      .where({
-        status: 1
-      })
-      .order('create_time desc').select();
+    const type = Number(this.get('type'));
+    const user = await this.session('data');
 
-    return this.success(groupList);
+    if (type === 2) {
+      // 列出我发起的所有订单团
+      const groupList = await this.model('group')
+        .where({
+          composer_user_id: user.id
+        })
+        .order('create_time desc').select();
+
+      return this.success(groupList);
+    } else if (type === 3) {
+      // 列出我参与的所有订单团
+      let groupList = await this.model('group')
+        .order('create_time desc').select();
+
+      groupList = groupList.filter(group => group.orders.map(o => o.user_id).includes(user.id));
+
+      return this.success(groupList);
+    } else {
+      // 按创建时间倒序排列，只列出征集中的团组
+      const groupList = await this.model('group')
+        .where({
+          status: 1
+        })
+        .order('create_time desc').select();
+
+      return this.success(groupList);
+    }
+
   }
 
   // 修改订单团状态
